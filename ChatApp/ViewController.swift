d8386be8f7539cfa1996d346a4a33523b63f1e32
Vanderlei Thome <vanderleithome@icloud.com>
2022-02-03 06:27:00 -0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Vanderlei Thome on 03/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
